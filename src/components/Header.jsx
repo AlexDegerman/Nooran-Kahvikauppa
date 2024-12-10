a99e94d72118176e2f874af5fcc55a250d3eb086
AlexDegerman <alex.degerman.dev@gmail.com>
2024-12-11 00:04:38 +0200

    Add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import '../styles/Header.css'
+
+const Header = () => {
+
+  return (
+    <header>
+      <h2 className="header-title">Nooran Kahvikauppa</h2>
+      <div className="header-nav-container">
+        <p className="header-nav">Etusivu</p>
+        <p className="header-nav">Kahvilaitteet</p>
+        <p className="header-nav">Kulutus laitteet</p>
+      </div>
+    </header>
+  )
+}
+
+export default Header
